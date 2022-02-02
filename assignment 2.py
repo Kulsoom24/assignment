caf0edcc39507f1294c006e411565c6c89fa901a
Kulsoom24 <98824955+Kulsoom24@users.noreply.github.com>
2022-02-02 16:27:55 +0530

    day 2,assignment 2

---
+++ assignment 2.py
@@ -0,0 +1,6 @@
+y = input("god of the drinks,'type the alphabet'")
+
+if y=='g' or y=='f' or  y=='d' or y=='t' or y=='o' or y=='i' or y=='k':
+    print('Congratulations you won the lottery')
+else:
+    print('Better luck next time')
